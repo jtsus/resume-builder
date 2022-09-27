873d96eb3cafa17e333f3061be23b5fbf9e76dc6
Justin Schreiber <justin.s@tubbo.net>
2022-09-27 13:22:04 -0700

    More CI work

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$SSH_HOST" ]; then
+  echo "SSH_HOST is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$SSH_PORT" ]; then
+  echo "SSH_PORT is not set. Defaulting to 22."
+  $SSH_PORT = 22
+fi
+
+if [ -z "$SSH_PASSWORD" ]; then
+  echo "SSH_PASSWORD is not set. Quitting."
+  exit 1
+fi
